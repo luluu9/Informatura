e0bc0319f29c8594cc92b82ff3fd0d5db3c64a83
Mateusz Naklicki <mateusz.naklicki06@gmail.com>
2021-06-08 22:13:27 +0200

    Add new page for uploading files

---
+++ web/wyslij.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<?php
+$title = 'INFORMATURA – kontakt';
+$desc = 'Wszystkie drogi komunikacji z twórcą tej strony.';
+?>
+
+<html lang="pl-PL">
+
+<head>
+    <?php include('php/meta.php') ?>
+    <link rel="stylesheet" type="text/css" href="/css/upload.css" media="all"> 
+    <script
+			  src="https://code.jquery.com/jquery-3.6.0.slim.min.js"
+			  integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI="
+			  crossorigin="anonymous"></script>
+    <script>
+        $(document).on('change', "#upload-form input[type='file']", function(){
+        if ($(this).val()) {
+
+            var filename = $(this).val().split("\\");
+         
+            filename = filename[filename.length-1];
+
+            $('#upload-form label').text(filename);
+        }
+ });
+    </script>
+</head>
+
+<body>
+    <?php include('php/header.php') ?>
+    <main>
+        <div id="big-title" class="center">
+            <h1>
+                Wyślij
+            </h1>
+        </div>
+        <div id="content" class="main-width">
+        W tym miejscu możesz podzielić się swoimi rozwiązaniami zadań. Zostaną one sprawdzone, a później udostępnione na stronie.
+            <form id='upload-form' method='POST' class='max-width'>
+                <div id='upload-details'>
+                    
+                    <input type='file' name='upload-file' id='upload-file'>
+                    <label for="upload-file">Wybierz plik</label>
+    
+                    <input type='text' id='upload-author' name='upload-author' placeholder='Autor'>
+                </div>    
+                <input type='text' id='upload-sheet' name='upload-sheet' placeholder='Rok matury, rodzaj...'>
+                <input type='text' id='upload-info' name='upload-info' placeholder='Dodatkowe informacje, uwagi...'>
+                <input type='submit' value='Wyślij' name='submit'>
+            </form>
+        </div>
+    </main>
+    <?php include('php/footer.php') ?>
+</body>
+</html>
