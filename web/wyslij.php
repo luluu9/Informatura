cb3852d65b5b671edd5e97da07bb9f133d4cb647
Mateusz Naklicki <mateusz.naklicki06@gmail.com>
2021-06-08 23:41:54 +0200

    Check if an uploaded file is small enough

--- web/wyslij.php
+++ web/wyslij.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
 <?php
-$title = 'INFORMATURA – kontakt';
-$desc = 'Wszystkie drogi komunikacji z twórcą tej strony.';
+$title = 'INFORMATURA – wyślij';
+$desc = 'Udostępnianie rozwiązań zadań';
+$uploaddir = 'D:/INFORMATURA/strona/uploads/'; // set valid on deploy
 ?>
 
 <html lang="pl-PL">
@@ -24,22 +25,6 @@ $desc = 'Wszystkie drogi komunikacji z twórcą tej strony.';
     </script>
 </head>
 
-<?php
-$uploaddir = 'D:/INFORMATURA/strona/uploads/'; // set valid on deploy
-if (!empty($_FILES)){
-    $tmp_name = $_FILES['upload-file']['tmp_name'];
-    $uploaded_name = basename($_FILES['upload-file']['name']);
-    $ext = strtolower(substr($uploaded_name, strripos($uploaded_name, '.')+1));
-    $new_name = hash_file('sha256', $tmp_name ) . '.' . $ext;
-    move_uploaded_file($tmp_name, $uploaddir . $new_name);
-}
-/*
-echo '<pre>';
-print_r($_FILES);
-print "</pre>";
-*/
-?>
-
 <body>
     <?php include('php/header.php') ?>
     <main>
@@ -49,9 +34,10 @@ print "</pre>";
             </h1>
         </div>
         <div id="content" class="main-width">
-        W tym miejscu możesz podzielić się swoimi rozwiązaniami zadań. Zostaną one sprawdzone, a później udostępnione na stronie.
+        W tym miejscu możesz podzielić się swoimi rozwiązaniami zadań. Zostaną one sprawdzone, a później udostępnione na stronie. Rozmiar pliku jest ograniczony do 5 MB.
             <form id='upload-form' method='post' enctype=multipart/form-data class='max-width'>
                 <div id='upload-details'>  
+                    <input type="hidden" name="max_file_size" value="5242880">
                     <input type='file' name='upload-file' id='upload-file'>
                     <label for="upload-file">Wybierz plik</label>
                     <input type='text' id='upload-author' name='upload-author' placeholder='Autor'>
@@ -60,6 +46,31 @@ print "</pre>";
                 <input type='text' id='upload-info' name='upload-info' placeholder='Dodatkowe informacje, uwagi...'>
                 <input type='submit' value='Wyślij' name='submit'>
             </form>
+            <?php
+                if (!empty($_FILES)) {
+                    if ($_FILES['upload-file']['error'] == UPLOAD_ERR_OK) {
+                        $tmp_name = $_FILES['upload-file']['tmp_name'];
+                        $uploaded_name = basename($_FILES['upload-file']['name']);
+                        $ext = strtolower(substr($uploaded_name, strripos($uploaded_name, '.')+1));
+                        $new_name = hash_file('sha256', $tmp_name ) . '.' . $ext;
+                        move_uploaded_file($tmp_name, $uploaddir . $new_name);
+                    }
+                    else if ($_FILES['upload-file']['error'] == UPLOAD_ERR_INI_SIZE || $_FILES['upload-file']['error'] == UPLOAD_ERR_FORM_SIZE)  { 
+                        // set maximum file size in the php.ini file: upload_max_filesize 
+                        $max_filesize = ini_get("upload_max_filesize");
+                        $max_filesize = (int) filter_var($max_filesize, FILTER_SANITIZE_NUMBER_INT); 
+                        echo "Plik jest za duży. Maksymalny rozmiar wynosi " . $max_filesize . " MB";
+                    }
+                    else {
+                        echo "Coś poszło nie tak. Spróbuj ponownie";
+                    }
+                }
+                /*
+                echo '<pre>';
+                print_r($_FILES);
+                print "</pre>";
+                */
+            ?>
         </div>
     </main>
     <?php include('php/footer.php') ?>
